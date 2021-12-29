3f32040049d7cdefb3316779b6b733a06c21f873
Andrew Welc <endriuwelc@gmail.com>
2021-12-29 20:59:18 +0100

    init commit

---
+++ components/LoginScreen.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import backgroundPic from "../public/images/bg-photo.jpg";
+import logoPic from "../public/images/eth-logo.png";
+import { useMoralis } from "react-moralis";
+
+function LoginScreen() {
+    const { authenticate } = useMoralis();
+    return (
+        <div className="bg-black relative">
+            <h1>Login screen</h1>
+            <div className="flex flex-col absolute z-50 h-4/6 items-center justify-center w-full space-y-6">
+                <Image src={logoPic} className="object-cover rounded-full" width={200} height={200} />
+                <button onClick={authenticate} className="bg-green-500 rounded-lg p-5 font-bold animate-bounce text-yellow-50">Login to the APP</button>
+            </div>
+
+            <div className="w-full h-screen">
+                <Image src={backgroundPic} layout="fill" objectFit="cover" />
+            </div>
+        </div>
+    )
+}
+
+export default LoginScreen
